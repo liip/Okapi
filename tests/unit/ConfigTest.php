552e5d3432adba7d7ac69b555e5080a6d444da3e
Patrice Neff <www-github@patrice.ch>
2007-12-03 17:43:31 +0000

    Configuration class loads configuration file based on environment variable OKAPI_ENV

---
+++ tests/unit/ConfigTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Tests the api_config class which handles reading of configuration
+ * XML files.
+ */
+class ConfigTest extends UnitTestCase {
+    function setUp() {
+        unset($_SERVER['OKAPI_ENV']);
+    }
+    
+    function tearDown() {
+        unset($_SERVER['OKAPI_ENV']);
+    }
+    
+    function testDefault() {
+        $cfg = new api_config();
+        $this->assertEqual($cfg->config_test, 'main');
+    }
+
+    function testEnvDev() {
+        $_SERVER['OKAPI_ENV'] = 'debug';
+        $cfg = new api_config();
+        $this->assertEqual($cfg->config_test, 'debug');
+    }
+
+    /**
+     * Test if XIncludes work.
+     */
+    function testEnvXInclude() {
+        $_SERVER['OKAPI_ENV'] = 'debug';
+        $cfg = new api_config();
+        $this->assertEqual($cfg->commandmap['@regex'], 'true');
+    }
+}
+?>
