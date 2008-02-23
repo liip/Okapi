727ef636b2d13396315d746b480aebec6252f7cf
Patrice Neff <www-github@patrice.ch>
2008-02-23 22:26:30 +0000

    Test case for api_helpers_class

---
+++ tests/unit/HelperClass.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Tests the api_helpers_class class.
+ */
+class HelperClassTest extends UnitTestCase {
+    /**
+     * Test that api_helpers_class::getBasename() works for strings.
+     */
+    function testGetBasenameFromString() {
+        $this->assertEqual('class', api_helpers_class::getBasename('my_test_class'));
+        $this->assertEqual('first', api_helpers_class::getBasename('first'));
+    }
+
+    /**
+     * Test that api_helpers_class::getBasename() works for an object.
+     */
+    function testGetBasenameFromObject() {
+        $this->assertEqual('HelperClassTest', api_helpers_class::getBasename($this));
+    }
+}
+?>
