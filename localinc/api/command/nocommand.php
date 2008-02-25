99103342083848d8648781dfa97c9357b209af1e
Patrice Neff <www-github@patrice.ch>
2008-02-25 15:35:37 +0000

    Rename api_commands_nocommand to api_command_nocommand

--- localinc/api/command/nocommand.php
+++ localinc/api/command/nocommand.php
@@ -1,5 +1,5 @@
 <?php
-class api_commands_nocommand extends api_command {
+class api_command_nocommand extends api_command {
     public function __construct($attribs) {
         parent::__construct($attribs);
     }
