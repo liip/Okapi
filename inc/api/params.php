0e06e3d431a6bdc27a99812d4e1e53ea08053f15
Marc Ammann <marc@codesofa.com>
2008-02-28 16:33:49 +0000

    - added support for: getParameters->get()/post() now accepts a parameter to get a specific parameter

--- inc/api/params.php
+++ inc/api/params.php
@@ -1,40 +1,52 @@
 <?php
 class api_params extends ArrayObject {
-	private $post = Array();
-	private $get  = Array();
-	
-	
-	public function __construct($array = Array()) {
-		parent::__construct($array, ArrayObject::ARRAY_AS_PROPS);
-	}
-	
-	/**
-	 * Set POST Data
-	 */
-	public function setPost($array) {
-		$this->post = $array;
-		$this->exchangeArray(array_merge($this->getArrayCopy(), $array));
-	}
-	
-	/**
-	 * Set GET Data
-	 */
-	public function setGet($array) {
-		$this->get = $array;
-		$this->exchangeArray(array_merge($this->getArrayCopy(), $array));
-	}
-	
-	/**
-	 * Get POST Data
-	 */
-	public function post() {
-		return $this->post;
-	}
-	
-	/**
-	 * Get GET Data
-	 */
-	public function get() {
-		return $this->get;
-	}
+    private $post = Array();
+    private $get  = Array();
+    
+    
+    public function __construct($array = Array()) {
+        parent::__construct($array, ArrayObject::ARRAY_AS_PROPS);
+    }
+    
+    /**
+     * Set POST Data
+     */
+    public function setPost($array) {
+        $this->post = $array;
+        $this->exchangeArray(array_merge($this->getArrayCopy(), $array));
+    }
+    
+    /**
+     * Set GET Data
+     */
+    public function setGet($array) {
+        $this->get = $array;
+        $this->exchangeArray(array_merge($this->getArrayCopy(), $array));
+    }
+    
+    /**
+     * Get POST Data
+     */
+    public function post($param = null) {
+        if (isset($param)) {
+            if (empty($this->post[$param])) {
+                return FALSE;
+            }
+            return $this->post[$param];
+        }
+        return $this->post;
+    }
+    
+    /**
+     * Get GET Data
+     */
+    public function get($param = null) {
+        if (isset($param)) {
+            if (empty($this->get[$param])) {
+                return FALSE;
+            }
+            return $this->get[$param];
+        }
+        return $this->get;
+    }
 }
