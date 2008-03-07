58960ba890b9f075604ff1262a6bb3c2943d622b
Marc Ammann <marc@codesofa.com>
2008-03-07 17:03:48 +0000

    - added new zend auth handler. only ldap for now..

---
+++ inc/api/pam/auth/zend.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once('Zend/Auth.php');
+
+class api_pam_auth_zend extends api_pam_common  implements api_pam_Iauth {
+  
+    /**
+     * Enter description here...
+     *
+     * @var Zend_Auth
+     */
+    private static $zaAuth;
+    
+    /**
+     * Enter description here...
+     *
+     * @var Zend_Auth_Adapter_Interface
+     */
+    private static $zaAdapter;
+    
+    public function __construct($opts) {
+        parent::__construct($opts);
+        
+        self::$zaAuth = Zend_Auth::getInstance();
+        self::$zaAuth->setStorage(new Zend_Auth_Storage_Session('Okapi_Auth'));
+    }
+    
+    public  function checkAuth() {
+        if (self::$zaAuth->hasIdentity()) {
+            return TRUE;
+        } else if (isset(self::$zaAdapter)) {
+            $zaResult = self::$zaAuth->authenticate(self::$zaAdapter);
+            
+            return $zaResult->isValid();
+        }
+        
+        return FALSE;
+    }
+    
+    public  function getAuthData() {
+      
+    }
+    
+    public  function getUserId() {
+    
+    }
+    
+    
+    public  function getUserName() {
+        return self::$zaAuth->getIdentity();
+    }
+    
+    public  function login($user, $pass) {
+        $rgOpts = $this->opts['container'];
+        
+        $strAdapter = $rgOpts['driver'];
+        
+        switch($strAdapter) {
+            case "ldap":
+                // TODO: Omg this is very ugly
+                foreach ($rgOpts as $host => $opts) {
+                    if ($host != "driver") {
+                        self::$zaAdapter = new Zend_Auth_Adapter_Ldap(Array($host => $opts), $user, $pass);
+                    }
+                }
+                break;
+            default:
+                error_log("OKAPI: Zend_Auth_Adapter_".$strAdapter." not yet usable in Okapi");
+                return FALSE;
+        }
+        
+        return $this->checkAuth();
+        
+    }
+    
+    public  function logout() {
+        self::$zaAuth->clearIdentity();
+    }
+}
