f66573a5eb1983d8c0fb933ee24ac7f65a45f72a
alainp <alain@liip.ch>
2008-03-13 09:45:45 +0000

    added basic documentation

--- inc/api/pam/auth/zend.php
+++ inc/api/pam/auth/zend.php
@@ -1,69 +1,95 @@
 <?php
+/**
+ * Implements a PAM authentication class using the Zend Auth
+ * classes.
+ * @see http://framework.zend.com/manual/zend.auth.html
+ */
 
 require_once('Zend/Auth.php');
 
 class api_pam_auth_zend extends api_pam_common  implements api_pam_Iauth {
 
     /**
-     * Enter description here...
-     *
      * @var Zend_Auth
      */
-    private static $zaAuth;
+    protected static $zaAuth = null;
 
     /**
-     * Enter description here...
-     *
-     * @var Zend_Auth_Adapter_Interface
+     * @var Zend_Auth_Adapter
      */
     private static $zaAdapter;
-
+    
+    /**
+     * Constructor.
+     */
     public function __construct($opts) {
         parent::__construct($opts);
-
         self::$zaAuth = Zend_Auth::getInstance();
         self::$zaAuth->setStorage(new Zend_Auth_Storage_Session('Okapi_Auth'));
     }
+    
+    /**
+     * @return boolean true if authenticated successfuly
+     * @see Zend_Auth_Result
+     */
 
     public  function checkAuth() {
         if (self::$zaAuth->hasIdentity()) {
-            return TRUE;
+            return true;
         } else if (isset(self::$zaAdapter)) {
-
             $zaResult = self::$zaAuth->authenticate(self::$zaAdapter);
-            
             $msg = $zaResult->getMessages();
-
-            // @see Zend_Auth_Result
             if($zaResult->getCode() !== 1){
                 throw new api_exception_Auth(api_exception::THROW_FATAL, array(), 0, $msg[1]);
             }
-
             return $zaResult->isValid();
         }
-
-        return FALSE;
+        return false;
     }
 
+    /**
+     * Returns the identity from storage or null if no identity is available
+     *
+     * @return mixed|null
+     * @todo is supposed to only return the ID, instead of the whole identity
+     */
     public  function getAuthData() {
-
-    }
-
-    public  function getUserId() {
-
-    }
-
-
-    public  function getUserName() {
         return self::$zaAuth->getIdentity();
     }
 
+    /**
+     * Returns the identity from storage or null if no identity is available
+     *
+     * @return mixed|null
+     * @todo is supposed to only return the ID, instead of the whole identity
+     */
+    public  function getUserId() {
+        return self::$zaAuth->getIdentity();
+    }
+
+    /**
+     * Returns the identity from storage or null if no identity is available
+     *
+     * @return mixed|null
+     * @todo is supposed to only return the ID instead of the whole identidy
+     */
+    public  function getUserName() {
+        return self::$zaAuth->getIdentity();
+        
+    }
+
+    /**
+     * Trigger the Login-Process
+     *
+     * @param String $user Username
+     * @param String $pass Password
+     * @return boolean true if authenticated successfuly
+     * @todo clean up the "very ugly stuff"
+     */
     public  function login($user, $pass) {
         self::$zaAuth->clearIdentity();
         $rgOpts = $this->opts['container'];
-
         $strAdapter = $rgOpts['driver'];
-
         switch($strAdapter) {
             case "ldap":
                 // TODO: Omg this is very ugly
@@ -75,13 +101,16 @@ class api_pam_auth_zend extends api_pam_common  implements api_pam_Iauth {
                 break;
             default:
                 error_log("OKAPI: Zend_Auth_Adapter_".$strAdapter." not yet usable in Okapi");
-                return FALSE;
+                return false;
         }
-
         return $this->checkAuth();
-
     }
-
+    
+    /**
+     * Clears the identity from persistent storage
+     *
+     * @return void
+     */
     public  function logout() {
         self::$zaAuth->clearIdentity();
     }
