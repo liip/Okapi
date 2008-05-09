5d5c5e8ed2ff00d5399654f6ad0b7b03d8cdc3b4
chregu <me@chregu.tv>
2008-05-09 05:23:47 +0000

    move inc et al. out of docroot make www/ the new docroot

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . '/inc/api/init.php';
+require_once dirname(__FILE__) . '/../inc/api/init.php';
 api_init::start();
 
 require_once API_LIBS_DIR.'controller.php';
