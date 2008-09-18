22d6ac2be95cddc559d6364c24261c799c13c0cc
Patrice Neff <www-github@patrice.ch>
2008-09-18 05:40:56 +0000

    Initial api_factory class with documentation of what I'm trying to achieve.

---
+++ inc/api/factory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Inversion of control container for Okapi. Returns initialized objects
+ * based on its basename. Has three strategies to initialize objects:
+ *    1. Completely from configuration - Both the actual class name and all
+ *       constructor parameters.
+ *    2. Partial configuration - Class name form configuration and
+ *       constructor parameters from the callee.
+ *    3. Default - Class name is the base name prepended with "api_" and
+ *       constructor parameters from the callee.
+ *
+ * This factory should get initialized by conf/classes.php.
+ */
+class api_factory {
+    
+}
