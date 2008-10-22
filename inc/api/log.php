e7f67c03297f1f9aa9d4804f249475a4e55dd19d
chregu <me@chregu.tv>
2008-10-22 09:47:51 +0000

    WS only

--- inc/api/log.php
+++ inc/api/log.php
@@ -6,28 +6,29 @@
  * The configured logger is available through api_log::$logger.
  */
 class api_log {
-    
-    const EMERG   = 0;  // Emergency: system is unusable
-    const ALERT   = 1;  // Alert: action must be taken immediately
-    const CRIT    = 2;  // Critical: critical conditions
-    const ERR     = 3;  // Error: error conditions
-    const WARN    = 4;  // Warning: warning conditions
-    const NOTICE  = 5;  // Notice: normal but significant condition
-    const INFO    = 6;  // Informational: informational messages
-    const DEBUG   = 7;  // Debug: debug messages
-    
+
+    const EMERG = 0; // Emergency: system is unusable
+    const ALERT = 1; // Alert: action must be taken immediately
+    const CRIT = 2; // Critical: critical conditions
+    const ERR = 3; // Error: error conditions
+    const WARN = 4; // Warning: warning conditions
+    const NOTICE = 5; // Notice: normal but significant condition
+    const INFO = 6; // Informational: informational messages
+    const DEBUG = 7; // Debug: debug messages
+
+
     /** Log: Configured logger. */
     public static $logger = null;
-    
+
     /** api_log instance */
     public static $instance = null;
-    
+
     /** lowest priority **/
     protected $priority = null;
-    
+
     /** MockWriter for testing */
     public static $mockWriter = null;
-    
+
     /**
      * Log a message if a logger is configured
      */
@@ -36,7 +37,7 @@ class api_log {
             return false;
         }
 
-        if (self::$instance === null ) {
+        if (self::$instance === null) {
             $config = api_config::getInstance()->log;
             if (empty($config) || !(self::$instance = api_log::getInstance())) {
                 self::$instance = false;
@@ -47,9 +48,9 @@ class api_log {
         $params = func_get_args();
         array_shift($params);
 
-        return self::$instance->logMessage($params,$prio);
+        return self::$instance->logMessage($params, $prio);
     }
-    
+
     /**
      * Initialize the logger.
      */
@@ -58,15 +59,15 @@ class api_log {
             // Already initialized
             return;
         }
-        
+
         $configs = api_config::getInstance()->log;
-        
+
         if (empty($configs[0]['class'])) {
             // Logging is not activated
-             self::$logger = false;
-             return;
+            self::$logger = false;
+            return;
         }
-        
+
         self::$logger = new Zend_Log();
 
         foreach ($configs as $cfg) {
@@ -77,7 +78,7 @@ class api_log {
             self::$logger->addWriter($log);
         }
     }
-    
+
     /**
      * Gets an instance of api_log.
      * @param $forceReload bool: If true, forces instantiation of a
@@ -85,14 +86,14 @@ class api_log {
      * @return api_log an api_log instance;
      */
     public static function getInstance($forceReload = FALSE) {
-        if (! self::$instance instanceof api_log || $forceReload) {
+        if (!self::$instance instanceof api_log || $forceReload) {
             self::$logger = null;
             self::$instance = new api_log();
         }
 
         return self::$instance;
     }
-    
+
     public function __call($method, $params) {
         $prio = self::getMaskFromLevel($method);
         $this->logMessage($params, $prio);
@@ -105,43 +106,42 @@ class api_log {
     public function getPriority() {
         return $this->priority;
     }
-    
+
     protected function createLogObject($name, $config) {
         $classname = 'Zend_Log_' . $name;
         $params = (array) (isset($config['cfg']) ? $config['cfg'] : array());
-        
+
         if (empty($params)) {
             $object = new $classname();
         } else {
             $class = new ReflectionClass($classname);
             $object = $class->newInstanceArgs($params);
         }
-        
+
         if (isset($config['priority'])) {
             $prio = $this->getMaskFromLevel($config['priority']);
             $object->addFilter(new Zend_Log_Filter_Priority($prio));
             if ($prio > $this->priority || !$this->priority) {
-                $this->priority = $prio;   
+                $this->priority = $prio;
             }
         }
-        
+
         return $object;
     }
-    
+
     /**
      * Return a api_log mask for a string level.
      */
     protected function getMaskFromLevel($level) {
         $masks = array(
-            'EMERG'   => api_log::EMERG,
-            'ALERT'   => api_log::ALERT,
-            'CRIT'    => api_log::CRIT,
-            'ERR'     => api_log::ERR,
-            'WARN'    => api_log::WARN,
-            'NOTICE'  => api_log::NOTICE,
-            'INFO'    => api_log::INFO,
-            'DEBUG'   => api_log::DEBUG,
-        );
+                'EMERG' => api_log::EMERG,
+                'ALERT' => api_log::ALERT,
+                'CRIT' => api_log::CRIT,
+                'ERR' => api_log::ERR,
+                'WARN' => api_log::WARN,
+                'NOTICE' => api_log::NOTICE,
+                'INFO' => api_log::INFO,
+                'DEBUG' => api_log::DEBUG);
         return $masks[strtoupper($level)];
     }
 
@@ -158,6 +158,6 @@ class api_log {
             $prio = self::INFO;
         }
 
-        return self::$logger->log($message,$prio);
+        return self::$logger->log($message, $prio);
     }
 }
