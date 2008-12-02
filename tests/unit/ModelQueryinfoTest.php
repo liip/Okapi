9561344e97163b467faa37265e5787458bd435c5
pjezek <patrick@jezek.ch>
2008-12-02 16:14:46 +0000

    okapi-test: - added testcase for silently ignored DOM_INVALID_CHARACTER_ERR in api_model_queryinfo

--- tests/unit/ModelQueryinfoTest.php
+++ tests/unit/ModelQueryinfoTest.php
@@ -48,4 +48,20 @@ class ModelQueryinfoTest extends api_testing_case_unit {
         $dom = $model->getDOM();
         $this->assertText($dom, '/queryinfo/requestURI', 'mycommand/foo');
     }
+    
+    function testWrongParams() {
+        $_GET = array('123' => 'wrongParam', 'param2' => 'validParam');
+        $request = api_request::getInstance(true);
+        $route = array('command' => 'mycommand', 'method' => 'foo');
+        
+        $model = new api_model_queryinfo($request, $route);
+        $dom = $model->getDOM();
+        
+        $this->assertNotNode($dom, '/queryinfo/query/123');
+        $this->assertText($dom, '/queryinfo/query/param2', 'validParam');
+        $this->assertText($dom, '/queryinfo/requestURI', 'mycommand/foo?0=wrongParam&param2=validParam');
+        $this->assertText($dom, '/queryinfo/lang', 'en');
+        $this->assertText($dom, '/queryinfo/command', 'mycommand');
+        $this->assertText($dom, '/queryinfo/method', 'foo');
+    }
 }
