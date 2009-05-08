744930ca06a35ed2144679a02386feb1b38e0f86
triggertoo <triggertoo@gmail.com>
2009-05-08 08:09:35 +0000

    additional stuff is now multiple-entry capable

--- inc/api/model/array.php
+++ inc/api/model/array.php
@@ -34,4 +34,8 @@ class api_model_array extends api_model {
         api_helpers_xml::array2dom($this->array, $dom, $dom->documentElement);
         return $dom;
     }
+    
+    public function getData() {
+    	return $this->array;
+    }
 }
