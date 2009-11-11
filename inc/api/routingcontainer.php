09b3a64d5f7010282d386d136455dc3621a1dd3f
lsmith77 <smith@pooteeweet.org>
2009-11-11 17:29:41 +0000

    routes should be loadable multiple times

--- inc/api/routingcontainer.php
+++ inc/api/routingcontainer.php
@@ -3,6 +3,6 @@
 class api_routingcontainer {
     public function __construct($routing) {
         // Load routing configuration
-        require_once API_PROJECT_DIR . 'conf/commandmap.php';
+        require API_PROJECT_DIR . 'conf/commandmap.php';
     }
 }
