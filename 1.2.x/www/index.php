7b424f772fac358320664528685069ed7836a0e0
Jordi Boggiano <j.boggiano@seld.be>
2010-01-13 16:03:57 +0000

    revert 12908

--- 1.2.x/www/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once dirname(__FILE__) . '/../inc/api/init.php';
-api_init::start();
-
-require_once API_LIBS_DIR.'controller.php';
-$ctrl = new api_controller();
-$ctrl->process();
