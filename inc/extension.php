ba88de6232d2b26ced79f90f77c5c9f843976686
Alain Petignat <alain@liip.ch>
2011-07-22 21:39:48 +0200

    Importing Okapi-Extension form-xsl

---
+++ inc/extension.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package form_xsl
+ *
+ * Okapi Extension for pure xsl forms.
+ */
+
+return array(
+    'name'        => 'form-xsl',
+    'author'      => 'Fabian Vogler',
+    'maintainer'  => array('Fabian Vogler'),
+    'version'     => '1.0.0',
+    'okapi'       => '1.0.x',
+    'description' => 'Okapi Extension for pure xsl forms.',
+);
