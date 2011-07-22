48263181106384060b4128a56662c2b11cee9f2c
Alain Petignat <alain@liip.ch>
2011-07-22 21:39:47 +0200

    Importing Okapi-Extension ext-example

---
+++ inc/extension.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package ext_example
+ *
+ * A funky skeleton for your extension
+ * 
+ * @see https://fosswiki.liip.ch/display/OKAPI/Home OkAPI Documentation
+ */
+
+return array(
+    'name'        => 'ext-example',
+    'author'      => 'Scooby Doo',
+    'version'     => '0.1',
+    'description' => 'Some prose about your extension.',
+);
