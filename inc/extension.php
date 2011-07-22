22a577cacddddbc579765bd0c0445afea1f771b0
Alain Petignat <alain@liip.ch>
2011-07-22 21:39:50 +0200

    Importing Okapi-Extension request-crud

---
+++ inc/extension.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package request_crud
+ *
+ * Okapi CRUD Handler for handling REST-standard requests with a simple interface
+ * Also a parameter cleaner comes with this packages which can be used without crud
+ * but crud relies on that parameter thing 
+ *
+ * @see http://en.wikipedia.org/wiki/Representational_State_Transfer
+ * @see http://en.wikipedia.org/wiki/Create,_read,_update_and_delete
+ */
+
+return array(
+    'name'        => 'request-crud',
+    'author'      => 'Marc Ammann',
+    'version'     => '0.1',
+    'okapi'       => '0.2',
+    'description' => 'Okapi REST to CRUD converter and parameter parser.',
+);
