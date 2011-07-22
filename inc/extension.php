a191067e778c7e73d59becf8ece30c4cc35bb6e6
Alain Petignat <alain@liip.ch>
2011-07-22 21:39:50 +0200

    Importing Okapi-Extension view-html

---
+++ inc/extension.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package view_json
+ *
+ * Okapi View Extension for showing DOMData as JSON. Includes a helper and a view class
+ *
+ * @see http://pecl.php.net/package/json JSON package
+ * @see http://www.ibm.com/developerworks/xml/library/x-xml2jsonphp/ JSON helper
+ * @see http://www.json.org/ JSON introduction
+ */
+
+return array(
+    'name'        => 'view-html',
+    'author'      => 'Lukas Smith',
+    'maintainer'  => Array('Lukas Smith'),
+    'version'     => '0.9.0',
+    'okapi'       => '1.0.x',
+    'description' => 'Okapi View Extension for showing data as HTML.',
+);
